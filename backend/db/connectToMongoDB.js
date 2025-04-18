4bb4a46b86c53312f5e75b0bee8c275e72813610
senakaratss <karatasssena@gmail.com>
2025-04-18 19:47:28 +0300

    backend completed

---
+++ backend/db/connectToMongoDB.js
@@ -0,0 +1,11 @@
+import mongoose from "mongoose";
+
+const connectToMongoDB = async () => {
+  try {
+    const conn = await mongoose.connect(process.env.MONGO_DB_URI);
+    console.log("MongoDB connected successfully:", conn.connection.host);
+  } catch (error) {
+    console.log("Error connecting to MongoDB:", error.message);
+  }
+};
+export default connectToMongoDB;
